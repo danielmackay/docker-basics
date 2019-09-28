a808432df02a24e30e75dd630032304a9ab734e3
Daniel Mackay <daniel.mackay@live.com>
2019-09-29 09:35:40 +1000

    Finished part 3

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 // app.js
 
+const mysql = require('mysql');
 const express = require('express');
+
+const con = mysql.createConnection({
+    host: "mysql",
+    user: "root",
+    password: "Password1?",
+    //database: "Customers"
+});
+
+con.connect(function(err){
+    if (err)throw err;
+    console.log('Connected!');
+})
 
 const app = express();
 
 const port = process.env.PORT
+//const port = 3000
 
 app.get('/', (req, res) => res.send('Hello Daniel!'));
 
